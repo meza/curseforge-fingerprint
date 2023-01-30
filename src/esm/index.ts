804d49348c94ef23d0cc330b36575acd97928473
meza <meza@meza.hu>
2023-01-30 17:02:13 +0000

    fix: expanded export syntax

--- src/esm/index.ts
+++ src/esm/index.ts
@@ -3,10 +3,10 @@ import {createRequire} from 'module';
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const curseforge = createRequire(import.meta.url)('node-gyp-build')(path.resolve(__dirname, '../../'));
-
 export interface Curseforge {
   fingerprint: (filePath: string) => number;
 }
+
+const curseforge: Curseforge = createRequire(import.meta.url)('node-gyp-build')(path.resolve(__dirname, '../../'));
 
 export default curseforge as Curseforge;
