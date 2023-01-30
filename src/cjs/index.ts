f8f807546be0aabfb57df598d5c9f8f4b373e479
meza <meza@meza.hu>
2023-01-30 17:15:56 +0000

    fix: removing src from the distributable

--- src/cjs/index.ts
+++ src/cjs/index.ts
@@ -1,8 +1,7 @@
-const path = require('node:path');
-const curseforge = require('node-gyp-build')(path.resolve(__dirname, '../../'));
-
 export interface Curseforge {
   fingerprint: (filePath: string) => number;
 }
+const path = require('node:path');
+const curseforge: Curseforge = require('node-gyp-build')(path.resolve(__dirname, '../../'));
 
 module.exports = curseforge as Curseforge;
